ed76a1d8c9370b5cca36aca1a21a096cdfc01135
rinsuki <428rinsuki+git@gmail.com>
2026-03-09 06:40:14 +0900

    chore: more better verify.py
    now it also checks crc field to ensure we are doing things correctly

--- verify.py
+++ verify.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
-from typing import IO
 import zipfile
 import json
 import argparse
 import os.path
+import binascii
 
-def verify_stream(stream: IO[bytes], orig_path: str):
+def verify_stream(zf: zipfile.ZipFile, zip_path: str, orig_path: str):
+    info = zf.getinfo(zip_path)
     BLOCK_SIZE = 1024 * 1024
-    with open(orig_path, "rb") as f:
-        while True:
-            orig_block = f.read(BLOCK_SIZE)
-            new_block = stream.read(BLOCK_SIZE)
-            if orig_block != new_block:
-                raise ValueError(f"Stream {orig_path} does not match original")
-            if not orig_block:
-                break
-
+    crc = 0
+    with zf.open(info) as stream:
+        with open(orig_path, "rb") as f:
+            while True:
+                orig_block = f.read(BLOCK_SIZE)
+                zf_block = stream.read(BLOCK_SIZE)
+                crc = binascii.crc32(orig_block, crc)
+                if orig_block != zf_block:
+                    raise ValueError(f"Stream {zip_path} does not match original file {orig_path}")
+                if not orig_block:
+                    break
+    if crc != info.CRC:
+        raise ValueError(f"CRC mismatch for {zip_path}: expected {info.CRC:08x}, got {crc:08x}")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -27,10 +32,8 @@ def main():
         streams = json.load(f)
     with zipfile.ZipFile(args.mp4_file) as zf:
         for i, stream in enumerate(streams["streams"]):
-            with zf.open(f"streams.{i}/init.m4s", "r") as f:
-                verify_stream(f, os.path.join(os.path.dirname(args.streams_json), stream["init"]))
+            verify_stream(zf, f"streams.{i}/init.m4s", os.path.join(os.path.dirname(args.streams_json), stream["init"]))
             for j, segment in enumerate(stream["chunks"]):
-                with zf.open(f"streams.{i}/chunks/chunk.{j:06d}.m4s", "r") as f:
-                    verify_stream(f, os.path.join(os.path.dirname(args.streams_json), os.path.dirname(stream["init"]), segment))
+                verify_stream(zf, f"streams.{i}/chunks/chunk.{j:06d}.m4s", os.path.join(os.path.dirname(args.streams_json), os.path.dirname(stream["init"]), segment))
 if __name__ == "__main__":
     main()
